5dfad5c717225924364d3241b82e0766e5783f94
Lili Manrique <liliana.manrique1@gmail.com>
2019-12-03 18:37:11 -0700

    Get rid of unnecessary for loop

--- main.js
+++ main.js
@@ -2,11 +2,9 @@ var submitButton = document.getElementById('submit-btn');
 var inputs = document.querySelectorAll('.card_guess input');
 
 function enableSubmitButton () {
-  for (var i = 0; i < inputs.length; i++) {
     if (inputs[0].value !== '' && inputs[1].value !== '' && inputs[2].value !== ''
     && inputs[3].value !== '') {
       submitButton.classList.add('enable');
-    }
   }
 }
 
