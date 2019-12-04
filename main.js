f38a59676b2296c6cc1cdea16e3021f0e0952504
Lili Manrique <liliana.manrique1@gmail.com>
2019-12-03 18:54:02 -0700

    Alphabetize and organize js file

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
-var submitButton = document.getElementById('submit-btn');
 var clearButton = document.getElementById('clear-btn');
+var submitButton = document.getElementById('submit-btn');
 var inputs = document.querySelectorAll('.card_guess input');
+
+function clearForm() {
+  for (var i = 0; i < inputs.length; i++) {
+    inputs[i].value = '';
+  }
+}
 
 function enableSubmitButton () {
   if (inputs[0].value !== '' && inputs[1].value !== '' && inputs[2].value !== ''
@@ -16,15 +22,9 @@ function enableClearButton () {
   }
 }
 
+clearButton.addEventListener('click', clearForm);
+
 window.addEventListener('input', function () {
   enableSubmitButton();
   enableClearButton();
 });
-
-clearButton.addEventListener('click', clearForm);
-
-function clearForm() {
-  for (var i = 0; i < inputs.length; i++) {
-    inputs[i].value = '';
-  }
-}
