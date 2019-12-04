6eddbd9458b6158074840f12573f9d7ff3cddab6
Harry Borrelli <hborrelli@outlook.com>
2019-12-03 22:17:24 -0700

    Start working on variables and functions. Updated ID's on current guess span elements.

---
+++ main.js
@@ -0,0 +1,15 @@
+// Variables
+var minRange = document.getElementById('min-range');
+var maxRange = document.getElementById('max-range');
+var updateRangeBtn = document.getElementById('update-range');
+
+// Event Listeners
+updateRangeBtn.addEventListener('click', updateRange);
+
+//Functions
+function updateRange() {
+  // Validate both inputs
+  if (minRange.value !== '' && maxRange.value !== '') {
+    
+  }
+}
