cca79e9a97ed75128f6f7eebe81fff404900d150
Harry Borrelli <hborrelli@outlook.com>
2019-12-03 22:50:47 -0700

    Add validation to form and enable red borders if inputs are empty

--- main.js
+++ main.js
@@ -5,11 +5,33 @@ var updateRangeBtn = document.getElementById('update-range');
 
 // Event Listeners
 updateRangeBtn.addEventListener('click', updateRange);
+window.addEventListener('keyup', enableSetRangeBtn);
 
-//Functions
-function updateRange() {
-  // Validate both inputs
+// Enabling set range button
+function enableSetRangeBtn() {
   if (minRange.value !== '' && maxRange.value !== '') {
-    
+    updateRangeBtn.classList.add('enable');
+  }
+}
+
+// Set ranges
+function updateRange() {
+  // Variables
+  var currentMin = document.getElementById('current-min-range');
+  var currentMax = document.getElementById('current-max-range');
+
+  // Validate & set values
+  if (minRange.value !== '' && maxRange.value !== '') {
+    currentMin.innerText = minRange.value;
+    currentMax.innerText = maxRange.value;
+    updateRangeBtn.classList.add('enable');
+  }
+
+  if (minRange.value === '') {
+    minRange.style.border = "2px solid red";
+  }
+
+  if (maxRange.value === '') {
+    maxRange.style.border = "2px solid red";
   }
 }
