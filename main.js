b9de129983d2ec80f8e068225c8c66f6f6734603
Lili Manrique <liliana.manrique1@gmail.com>
2019-12-03 18:47:55 -0700

    Add clear form function to js

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
 var submitButton = document.getElementById('submit-btn');
+var clearButton = document.getElementById('clear-btn');
 var inputs = document.querySelectorAll('.card_guess input');
 
 function enableSubmitButton () {
-    if (inputs[0].value !== '' && inputs[1].value !== '' && inputs[2].value !== ''
-    && inputs[3].value !== '') {
-      submitButton.classList.add('enable');
+  if (inputs[0].value !== '' && inputs[1].value !== '' && inputs[2].value !== ''
+  && inputs[3].value !== '') {
+    submitButton.classList.add('enable');
   }
 }
 
-window.addEventListener('input', enableSubmitButton);
+function enableClearButton () {
+  if (inputs[0].value !== '' || inputs[1].value || '' || inputs[2].value || ''
+  || inputs[3].value !== '') {
+    clearButton.classList.add('enable');
+  }
+}
+
+window.addEventListener('input', function () {
+  enableSubmitButton();
+  enableClearButton();
+});
