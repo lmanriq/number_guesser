4aab2e04d2b7f586d1d74f6947e14d3d0b7cc4e1
Lili Manrique <liliana.manrique1@gmail.com>
2019-12-07 16:05:56 -0700

    Re-insert accidentally deleted line from win card function

--- main.js
+++ main.js
@@ -279,6 +279,7 @@ function gameWin() {
     </section>`;
     // winCardCounter++;
     // Insert win card into container
+    gameCardContainer.insertAdjacentHTML('afterbegin', winCardHTML);
   }
 }
 
